20f11a06e795d1c0f334556279e0eeba63ae7621
Jared Ramsey <Jared.Ramsey@vu.com>
2022-01-27 20:20:17 -0600

    overtime pay calc

--- payCalculator.py
+++ payCalculator.py
@@ -2,6 +2,18 @@ def calculatePay():
     
     # This first line is provided for you
     hrs = input("Enter Hours:")
+    rate = input("Enter Rate: ")
+
+    hrs = float(hrs)
+    rate = float(rate)
+
+    if hrs > 40.0:
+        pay = (rate * 40) + ((hrs - 40) * (rate * 1.5))
+    else:
+        pay = rate * hrs
+    
+    print("Pay: ", pay)
+
     
     # end assignment
 
@@ -9,4 +21,4 @@ def calculatePay():
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+## calculatePay()
